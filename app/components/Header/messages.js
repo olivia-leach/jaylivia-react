041f27e512bcf2a53d171bbdb21f57c0a623cdb1
olivia-leach <olivia.i.m.leach@gmail.com>
2017-09-20 21:46:08 -0400

    Hide hotels for now

--- app/components/Header/messages.js
+++ app/components/Header/messages.js
@@ -3,14 +3,14 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   home: {
     id: 'boilerplate.components.Header.home',
-    defaultMessage: 'HI',
+    defaultMessage: 'HI THERE',
   },
   where: {
     id: 'boilerplate.components.Header.where',
     defaultMessage: 'WHERE',
   },
-  hotels: {
-    id: 'boilerplate.components.Header.hotels',
-    defaultMessage: 'HOTELS',
-  },
+  // hotels: {
+  //   id: 'boilerplate.components.Header.hotels',
+  //   defaultMessage: 'HOTELS',
+  // },
 });
