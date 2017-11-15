3bf1d1371681ab01424d50d9d06c1a467272db0f
olivia-leach <olivia.i.m.leach@gmail.com>
2017-11-14 23:03:16 -0500

    More work on RSVP page

--- app/containers/RSVPPage/index.js
+++ app/containers/RSVPPage/index.js
@@ -5,63 +5,37 @@ export default class RSVPPage extends React.PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      name: '',
+      guest_count: '1',
+      first_name: '',
+      last_name: '',
+      first_name_2: '',
+      last_name_2: '',
       message: '',
       rsvp: '',
-      rsvpBrunch: '',
+      rsvp_brunch: '',
+      rsvp_drinks: '',
+      rsvp_rehersal: '',
+      rsvp_tubing: '',
     }
 
     this.submitForm = this.submitForm.bind(this)
-    this.tryGet = this.tryGet.bind(this)
-    this.setGuestCount = this.setGuestCount.bind(this)
+    // this.tryGet = this.tryGet.bind(this)
+    this.onInputChange = this.onInputChange.bind(this)
   }
 
   componentDidMount() {
     window.sr = ScrollReveal();
-    sr.reveal('.input', 50);
+    sr.reveal('.form-group', 50);
+    // sr.reveal('.form-check', 50);
+    // sr.reveal('label', 50);
   }
 
   // get all data in form and return object
   getFormData() {
-    const form = document.getElementById("gform");
-    let elements = form.elements; // all form elements
-    let fields = Object.keys(elements).map(function(k) {
-      if(elements[k].name !== undefined) {
-        return elements[k].name;
-      // special case for Edge's html collection
-      }else if(elements[k].length > 0){
-        return elements[k].item(0).name;
-      }
-    }).filter(function(item, pos, self) {
-      return self.indexOf(item) == pos && item;
-    });
-    let data = {};
-    fields.forEach(function(k){
-      data[k] = elements[k].value;
-      let str = ""; // declare empty string outside of loop to allow
-                    // it to be appended to for each item in the loop
-      if(elements[k].type === "checkbox"){ // special case for Edge's html collection
-        str = str + elements[k].checked + ", "; // take the string and append
-                                                // the current checked value to
-                                                // the end of it, along with
-                                                // a comma and a space
-        data[k] = str.slice(0, -2); // remove the last comma and space
-                                    // from the  string to make the output
-                                    // prettier in the spreadsheet
-      }else if(elements[k].length){
-        for(let i = 0; i < elements[k].length; i++){
-          if(elements[k].item(i).checked){
-            str = str + elements[k].item(i).value + ", "; // same as above
-            data[k] = str.slice(0, -2);
-          }
-        }
-      }
-    });
-
     // add form-specific values into the data
-    data.formDataNameOrder = JSON.stringify(fields);
-    data.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-    data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+    const data = this.state
+    data.formDataNameOrder = JSON.stringify(data);
+    data.formGoogleSheetName = 'responses'; // default sheet name
 
     console.log(data);
     return data;
@@ -81,30 +55,30 @@ export default class RSVPPage extends React.PureComponent {
     }
   }
 
-  tryGet(e) {
-    e.preventDefault();
-    const url = "https://script.google.com/macros/s/AKfycbz8Qm6DWJYMNA2_n6vol9JgiNi26gP63Q0cm7wOL573B-lD9AjY/exec?test=test&callback=?"
-    // const url = 'https://script.google.com/macros/s/AKfycbz8Qm6DWJYMNA2_n6vol9JgiNi26gP63Q0cm7wOL573B-lD9AjY/exec?firstName=bob&lastName=smith'
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function() {
-      console.log(xhr.status, xhr.statusText)
-      console.log(xhr.responseText);
-      return;
-    };
-
-    const data = {
-      firstName: 'hi',
-      lastName: 'bob'
-    }
-
-    // url encode form data for sending as post data
-    let encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-    }).join('&')
-    xhr.send(data => console.log(data))
-  }
+  // tryGet(e) {
+  //   e.preventDefault();
+  //   const url = "https://script.google.com/macros/s/AKfycbz8Qm6DWJYMNA2_n6vol9JgiNi26gP63Q0cm7wOL573B-lD9AjY/exec?test=test&callback=?"
+  //   // const url = 'https://script.google.com/macros/s/AKfycbz8Qm6DWJYMNA2_n6vol9JgiNi26gP63Q0cm7wOL573B-lD9AjY/exec?firstName=bob&lastName=smith'
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open('GET', url);
+  //   // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  //   xhr.onreadystatechange = function() {
+  //     console.log(xhr.status, xhr.statusText)
+  //     console.log(xhr.responseText);
+  //     return;
+  //   };
+  //
+  //   const data = {
+  //     firstName: 'hi',
+  //     lastName: 'bob'
+  //   }
+  //
+  //   // url encode form data for sending as post data
+  //   let encoded = Object.keys(data).map(function(k) {
+  //       return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+  //   }).join('&')
+  //   xhr.send(data => console.log(data))
+  // }
 
   submitForm(event) {
     event.preventDefault();
@@ -132,9 +106,12 @@ export default class RSVPPage extends React.PureComponent {
     xhr.send(encoded);
   }
 
-  setGuestCount(e) {
+  onInputChange(e) {
+    let val = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    if (val === true) { val = 'yes' }
+    if (val === false) { val = 'false' }
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: val
     })
   }
 
@@ -148,54 +125,85 @@ export default class RSVPPage extends React.PureComponent {
           ]}
         />
         <div className='content-wrapper rsvp-page'>
+          {this.state.submitted ?
+            <div>Thanks!</div>
+          :
           <form id='gform'>
             <div className="form-group">
               <label htmlFor="guestCount">Number of guests</label>
-              <select className="form-control" id="guestCount" name='guestCount' onChange={this.setGuestCount}>
+              <select className="form-control" id="guestCount" name='guest_count' onChange={this.onInputChange}>
                 <option>1</option>
                 <option>2</option>
               </select>
             </div>
             <div className="form-group">
-              {this.state.guestCount == 2 && <label htmlFor="guest1">Human 1</label>}
+              {this.state.guest_count == 2 && <label htmlFor="guest1">Human 1</label>}
               <div className='row' id='guest1'>
                 <div className='col'>
-                  <input type="text" className="form-control" placeholder="First name" aria-label="First Name" name='first_name' />
+                  <input type="text" className="form-control" placeholder="First name" aria-label="First Name" name='first_name' onChange={this.onInputChange} />
                 </div>
                 <div className='col'>
-                  <input type="text" className="form-control" placeholder="Last name" aria-label="Last Name" name='last_name' />
+                  <input type="text" className="form-control" placeholder="Last name" aria-label="Last Name" name='last_name' onChange={this.onInputChange} />
                 </div>
               </div>
             </div>
-            {this.state.guestCount == 2 &&
+            {this.state.guest_count == 2 &&
               <div className="form-group">
                 <label htmlFor="guest2">Human 2</label>
                 <div className='row' id='guest2'>
                   <div className='col'>
-                    <input type="text" className="form-control" placeholder="First name" aria-label="First Name" name='first_name' />
+                    <input type="text" defaultValue={this.state.first_name_2} className="form-control" placeholder="First name" aria-label="First Name" name='first_name_2' onChange={this.onInputChange} />
                   </div>
                   <div className='col'>
-                    <input type="text" className="form-control" placeholder="Last name" aria-label="Last Name" name='last_name' />
+                    <input type="text" defaultValue={this.state.last_name_2} className="form-control" placeholder="Last name" aria-label="Last Name" name='last_name_2' onChange={this.onInputChange} />
                   </div>
                 </div>
               </div>
             }
-            <label htmlFor="rsvp">We cannot wait to see you! Will you be joining us?</label>
-            <div className="form-check" id='rsvp'>
-              <label className="form-check-label">
-                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" /> Yay!
-              </label>
+            <div className='form-group'>
+              <p>We cannot wait to see you! Will you be joining us?</p>
             </div>
-            <div className="form-check form-check-inline">
-              <label className="form-check-label">
-                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" /> Nay
-              </label>
+            <div className='form-group'>
+              <div className="form-check form-check-inline">
+                <label className="form-check-label">
+                  <input className="form-check-input" type="radio" name="rsvp" id="inlineRadio1" value="yes" onChange={this.onInputChange} /> Yay!
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <label className="form-check-label">
+                  <input className="form-check-input" type="radio" name="rsvp" id="inlineRadio2" value="no" onChange={this.onInputChange} /> Nay
+                </label>
+              </div>
             </div>
+            {this.state.rsvp === 'yes' &&
+              <div>
+                <div className='form-group'>
+                  <p>Yessss! How about these fun activities?</p>
+                </div>
+                <div className='form-group'>
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input className="form-check-input" type="checkbox" name="rsvp_tubing" onChange={this.onInputChange} /> Tubing the Espopus River on Friday in Phoenicia
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input className="form-check-input" type="checkbox" name="rsvp_drinks" onChange={this.onInputChange} /> Welcome drinks on Friday night in Woodstock
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input className="form-check-input" type="checkbox" name="rsvp_brunch" onChange={this.onInputChange} /> Farewell brunch on Sunday morning at Onteora
+                    </label>
+                  </div>
+                </div>
+              </div>
+            }
             <div className="form-group">
-              <textarea className="form-control" name='message' id="messageText" rows="3" placeholder="Send us a note!"></textarea>
+              <textarea className="form-control" name='message' id="messageText" rows="3" placeholder="Send us a note!" onChange={this.onInputChange}></textarea>
             </div>
             <button className='btn btn-primary btn-block' type='submit' onClick={this.submitForm}>Submit</button>
-          </form>
+          </form>}
         </div>
       </article>
     );
